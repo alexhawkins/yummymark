85daac19deffa9e8ebab49e36f62da8b897b0aab
Alex Hawkins <alexhawkins.me@gmail.com>
2014-05-05 10:52:52 -0700

    yummymark setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yummymark::Application.config.secret_key_base = '86a4b3c26c3428a57c4b57b22a59b8e3dbeab229e622c93cbd8882b52894457876b404293385288e915a2aee8fb7d4e071c707e2f19bd642c89f7dfdc190fabd'
