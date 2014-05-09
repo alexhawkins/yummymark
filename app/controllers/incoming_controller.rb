c095563a760b11435e9336dfdbe6a7b790f79305
Alex Hawkins <alexhawkins.me@gmail.com>
2014-05-08 20:31:58 -0700

    Fix incoming mail

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,9 +9,44 @@ class IncomingController < ApplicationController
     puts "INCOMING PARAMS HERE: #{params}"
 
     # You put the message-splitting and business
-    # magic here. 
+    # magic here.
+    sender  = params['from']
+    subject = params['subject'] 
+    body_url = params["stripped-text"]
+
+    @bookmark.topics << subject
+    current_user.topics << subject 
+
+    @boookmark = Bookmark.new
+    @bookmark = current_user.bookmarks.build(bookmark_params)
+    @bookmark.url = body_url
+    #get the user input from the topic field and split the words into an array called topics
+    add_these_topics = subject.gsub(/[^a-zA-Z]/, ' ').downcase.split
+    #now iterate through each topic in our array and associate them with the bookmark we're creating
+      add_these_topics.each do |topic_title|
+        #find our title in our Topics class if it exists, otherwise create the topic
+        topic = Topic.find_or_create_by_title(topic_title)
+        #we then associate the topics with our bookmark
+        @bookmark.topics << topic
+        #check to make sure this topic has not already been associated with this user
+        unless current_user.topics.include?(topic)
+          #associate topics with user 
+          current_user.topics << topic
+        end
+      end
+
 
     # Assuming all went well. 
     head 200
   end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(
+      :title, :url, 
+      {:topic_ids => [] }
+    )
+  end
+
 end
